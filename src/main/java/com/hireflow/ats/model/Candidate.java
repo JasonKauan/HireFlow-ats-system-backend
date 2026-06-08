82a79a3ecadf7c3d3f452bcf54e8b37376d49e7a
JasonKauan <jasonkauandiass@gmail.com>
2026-06-08 18:59:44 -0300

    adicionado relacionamento OneToOne entre Candidate e Resume

--- src/main/java/com/hireflow/ats/model/Candidate.java
+++ src/main/java/com/hireflow/ats/model/Candidate.java
@@ -7,7 +7,6 @@ import lombok.*;
 
 @Data
 @Entity
-@Getter@Setter
 @Table(name = "candidates")
 public class Candidate {
 
@@ -15,16 +14,24 @@ public class Candidate {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne(mappedBy = "candidate")
+    private Resume resume;
+
     @Column (nullable = false)
     private String name;
+
+
     @Column(nullable = false, unique = true)
     private String email;
+
 
     @Column(nullable = false)
     private String phone;
 
+
     @Column(length = 2000)
     private String linkedinProfile;
+
 
     @Column(length = 2000)
     private String githubProfile;
